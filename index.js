255cf9f535c53bb95ac995ea6fb220b56d5e483e
WongJapan <kulomilyarder@gmail.com>
2022-05-23 03:59:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const fs = require("fs");
-const Parse = require("./helper/cli_parser");
-
-const args = Parse(process.argv);
-console.log(args);
-
-if (fs.existsSync(`${args.network}.json`)) {
-  console.log(`Ada`);
-}
+// silent
